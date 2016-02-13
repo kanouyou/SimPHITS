6425ce8ab9b74b187f898c1b26b6e1c0d3fa1318
YeYang <kanouyou@kune2a.nucl.kyushu-u.ac.jp>
2016-02-13 17:31:17 +0900

    update

--- script/CheckMakefile.py
+++ script/CheckMakefile.py
@@ -5,7 +5,7 @@ This is python script to extract some useful information from
 PHITS makefile and generate CMakeLists.txt
 
 Usage:
-     CheckMakefile.py [--phits_dir=<dir>]
+     CheckMakefile.py [--option]
 
 Option:
      --phits_dir      set the path of phits directory
@@ -20,7 +20,23 @@ phits_dir = "/home/yeyang/Documents/Software/PHITS/phits2.88"
 
 # check the version of this phits code
 def GetPhitsVersion(phitsdir):
-	return
+	version = 0.0
+	phits_main = phitsdir + "/src/main.f"
+	try:
+		mainfile = open(phits_main, "r")
+	except:
+		print "ERROR: Cannot open this file: ", phits_main
+		print "NOTE: Please make sure if this file exsits or not!!"
+	finally:
+		dataline = mainfile.readlines()
+		mainfile.close()
+		# check the version
+		for eachline in dataline:
+			if match("^\s+ parameter \( Version =", eachline):
+				eachline.strip()
+				item = eachline.split()
+				version = item[4]
+	return version
 
 # check the directory path and get contents of phits makefile
 def GetMakefileContents(phits_dir):
@@ -34,7 +50,8 @@ def GetMakefileContents(phits_dir):
 	try:
 		makefile = open(phits_makefile, "r")
 	except:
-		print "ERROR: Cannot open this file!! Make sure the directory is correct!!!"
+		print "ERROR: Cannot open this file: ", phits_makefile
+		print "NOTE: Please make sure if this file exsits or not!!"
 	finally:
 		dataline = makefile.readlines()
 		makefile.close()
@@ -42,7 +59,24 @@ def GetMakefileContents(phits_dir):
 
 # get source used in this version of phits
 def GetSourceInfo(dataline):
+	src = []
+	# read makefile contents
+	for i in range(len(dataline)):
+		if match("^SRCS[0-9]", dataline[i]):
+			# src buffer to contain the info
+			buff = []
+			# read next 10 lines after this line
+			for j in range(10):
+				# input info into buffer until found "^$"
+				if match("^$", dataline[i+j]):
+					break
+				buff.append(dataline[i+j])
+			src.append(buff)
+	for i in range(len(src)):
+		for j in range(len(src[i])):
+			print src[i][j]
 	return
+
 
 # ----------- main ------------- #
 if (__name__=="__main__"):
@@ -53,7 +87,9 @@ if (__name__=="__main__"):
 	# check the other option
 	for i in range(len(argv)):
 		if (argv[i]=="--version" or argv[i]=="-v"):
-			GetPhitsVersion(phits_dir)
+			version = GetPhitsVersion(phits_dir)
+			print " -- PHITS Version: ", version
 
 	print " -- PHITS Directory: ", phits_dir
 	contents = GetMakefileContents(phits_dir)
+	GetSourceInfo(contents)
